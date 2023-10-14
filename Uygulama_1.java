7a14b20e98313efbb7a29ff4918b11f770e21ff8
Arsalan Khroush <arsalan_khroush@yahoo.com>
2023-10-14 18:25:14 +0300

    Fix

--- Uygulama_1.java
+++ Uygulama_1.java
@@ -4,14 +4,14 @@ public class Uygulama_1 {
     public static void main(String[] args) {
         int sayi1, sayi2; //Sayılar
         Scanner scan = new Scanner(System.in);
-        System.out.println("ilk sayiyi girin:");
+        System.out.print("ilk sayiyi girin:");
         sayi1 = scan.nextInt();
-        System.out.println("ikinci sayiyi girin:");
+        System.out.print("ikinci sayiyi girin:");
         sayi2 = scan.nextInt();
         if (sayi1 > sayi2){
-            System.out.println("Buyuk sayi: " + sayi1);
+            System.out.print("Buyuk sayi: " + sayi1);
         } else if (sayi2 > sayi1) {
-            System.out.println("Buyuk sayi: " + sayi2);
+            System.out.print("Buyuk sayi: " + sayi2);
         }
     }
 }
